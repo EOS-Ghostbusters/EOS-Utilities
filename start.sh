8500cade699813785fb608782b044493c7d80455
Jae Chung <jae.chung99@gmail.com>
2018-08-28 15:09:42 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ NODEOS=/home/ubuntu/eos/build/programs/nodeos/nodeos
 DATADIR=/home/ubuntu/trusted
 ./stop.sh
 timestamp=$(/bin/date +%s)
-$NODEOS --data-dir $DATADIR --config-dir $DATADIR --verbose-http-errors --abi-serializer-max-time-ms 40 "$@" > stdout.txt 2> eos-$timestamp.log &  echo $! > nodeos.pid
+$NODEOS --data-dir $DATADIR --config-dir $DATADIR --verbose-http-errors "$@" > stdout.txt 2> eos-$timestamp.log &  echo $! > nodeos.pid
 /bin/rm -f eos.log
 /bin/ln -s eos-$timestamp.log eos.log
